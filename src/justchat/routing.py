5bfec2090b2453dba893afabf75e8a25484def8b
sirrotich <rotichtitus12@gmail.com>
2019-05-08 16:07:17 +0300

    create routing n the project

--- src/justchat/routing.py
+++ src/justchat/routing.py
@@ -2,6 +2,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
